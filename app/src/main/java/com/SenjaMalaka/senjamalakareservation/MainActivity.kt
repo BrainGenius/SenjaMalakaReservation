cd8813742f47f552efdb4f993fe1c2eebff33e5e
BrainGenius <andharuntf01@gmail.com>
2024-06-09 20:55:14 +0700

    andharu: Fix Button on Main Menu, Add Logic for an Active Menu to Change the Button Image

--- app/src/main/java/com/SenjaMalaka/senjamalakareservation/MainActivity.kt
+++ app/src/main/java/com/SenjaMalaka/senjamalakareservation/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.SenjaMalaka.senjamalakareservation
 
 import android.os.Bundle
-import android.view.View
+import android.widget.ImageButton
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,13 +10,15 @@ import androidx.core.view.WindowInsetsCompat
 import com.SenjaMalaka.senjamalakareservation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -26,39 +28,60 @@ class MainActivity : AppCompatActivity() {
         var menu = 0
 
         supportFragmentManager.beginTransaction().replace(R.id.Menu_Fragment, Menu_PemesananFragment()).commit()
+        setActiveButton(binding.ButtonMenuPemesanan)
 
-        binding.ButtonMenuPemesanan.setOnClickListener{
+        binding.ButtonMenuPemesanan.setOnClickListener {
             menu = 1
             supportFragmentManager.beginTransaction().replace(R.id.Menu_Fragment, Menu_PemesananFragment()).commit()
+            setActiveButton(binding.ButtonMenuPemesanan)
         }
-        binding.ButtonMenuSewaFotografer.setOnClickListener{
+        binding.ButtonMenuSewaFotografer.setOnClickListener {
             menu = 2
             supportFragmentManager.beginTransaction().replace(R.id.Menu_Fragment, Menu_SewaFotograferFragment()).commit()
+            setActiveButton(binding.ButtonMenuSewaFotografer)
         }
-        binding.ButtonMenuSewaLiveMusic.setOnClickListener{
+        binding.ButtonMenuSewaLiveMusic.setOnClickListener {
             menu = 3
             supportFragmentManager.beginTransaction().replace(R.id.Menu_Fragment, Menu_SewaLiveMusicFragment()).commit()
+            setActiveButton(binding.ButtonMenuSewaLiveMusic)
         }
-        binding.ButtonMenuSewaBeanBag.setOnClickListener{
+        binding.ButtonMenuSewaBeanBag.setOnClickListener {
             menu = 4
             supportFragmentManager.beginTransaction().replace(R.id.Menu_Fragment, Menu_SewaBeanBagFragment()).commit()
+            setActiveButton(binding.ButtonMenuSewaBeanBag)
         }
-        binding.ButtonMenuUpdateEvent.setOnClickListener{
+        binding.ButtonMenuUpdateEvent.setOnClickListener {
             menu = 5
             supportFragmentManager.beginTransaction().replace(R.id.Menu_Fragment, Menu_UpdateEventFragment()).commit()
+            setActiveButton(binding.ButtonMenuUpdateEvent)
         }
 
-        onBackPressedDispatcher.addCallback(object: OnBackPressedCallback(true) {
+        onBackPressedDispatcher.addCallback(object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
-                if (menu != 1)
-                {
+                if (menu != 1) {
                     binding.ButtonMenuPemesanan.performClick()
-                }
-
-                else {
+                } else {
                     finishAffinity()
                 }
             }
         })
+    }
+
+    private fun setActiveButton(activeButton: ImageButton) {
+        // Reset all button backgrounds to default
+        binding.ButtonMenuPemesanan.setBackgroundResource(R.drawable.menu_pemesanan)
+        binding.ButtonMenuSewaFotografer.setBackgroundResource(R.drawable.menu_sewafotografer)
+        binding.ButtonMenuSewaLiveMusic.setBackgroundResource(R.drawable.menu_sewalivemusic)
+        binding.ButtonMenuSewaBeanBag.setBackgroundResource(R.drawable.menu_sewabeanbag)
+        binding.ButtonMenuUpdateEvent.setBackgroundResource(R.drawable.menu_updateevent)
+
+        // Set the active button background to active drawable
+        when (activeButton.id) {
+            R.id.ButtonMenu_Pemesanan -> activeButton.setBackgroundResource(R.drawable.menu_pemesanan_active)
+            R.id.ButtonMenu_SewaFotografer -> activeButton.setBackgroundResource(R.drawable.menu_sewafotografer_active)
+            R.id.ButtonMenu_SewaLiveMusic -> activeButton.setBackgroundResource(R.drawable.menu_sewalivemusic_active)
+            R.id.ButtonMenu_SewaBeanBag -> activeButton.setBackgroundResource(R.drawable.menu_sewabeanbag_active)
+            R.id.ButtonMenu_UpdateEvent -> activeButton.setBackgroundResource(R.drawable.menu_updateevent_active)
+        }
     }
 }
